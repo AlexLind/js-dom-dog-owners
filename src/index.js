016bf4252f5544ad5ae4a7525bea7731dd52c418
Alexander Lind <alex.anonym@gmail.com>
2022-09-02 15:07:27 +0200

    finished instructions part 1

--- src/index.js
+++ src/index.js
@@ -12,55 +12,63 @@ const createSection = () => {
     return myDogSection
 }
 
-const createSectionHeader = () => {
+const createSectionHeader = (name) => {
     const header = document.createElement('h2')
-    header.innerText = data[0].name;
+    header.innerText = name;
     return header
 }
 
-const createSectionImage = () => {
+const createSectionImage = (imageSource) => {
     const image = document.createElement('img')
-    image.setAttribute('src', data[0].image)
+    image.setAttribute('src', imageSource)
     return image
 }
 
-const createDogCard = () => {
+const createDogCard = (dog) => {
+    console.log(dog)
     myMainSection.innerHTML = ""
     const section = createSection()
-    const header = createSectionHeader()
-    const image = createSectionImage()
-    const desc = createDogCardDesc()
+    const header = createSectionHeader(dog.name)
+    const image = createSectionImage(dog.image)
+    const desc = createDogCardDesc(dog.bio, dog.isGoodDog)
     
     section.append(header, image, desc)
     return section
 }
 
-const createDogCardDesc = () => {
+const createDogCardDesc = (bio, isGoodDog) => {
     const cardDesc = document.createElement('div')
     cardDesc.setAttribute('class', 'main__dog-section__desc')
     const header = document.createElement('h3')
     header.innerHTML = 'Bio'
     cardDesc.append(header)
     const text = document.createElement('p')
-    text.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    text.innerHTML = bio
     cardDesc.append(text)
     const bottomSection = document.createElement('div')
     bottomSection.setAttribute('class', 'main__dog-section__btn')
     cardDesc.append(bottomSection)
     const textBottom = document.createElement('p')
-    textBottom.innerHTML = '<em>Is naughty?</em> yes!'
     bottomSection.append(textBottom)
     const button = document.createElement('button')
-    button.innerHTML = 'Good Dog!'
+    if (isGoodDog) {
+        textBottom.innerHTML = '<em>Is naughty?</em> no!'
+        button.innerHTML = 'Good Dog!'
+    } else {
+        textBottom.innerHTML = '<em>Is naughty?</em> yes!'
+        button.innerHTML = 'Bad Dog!'
+        } 
     bottomSection.append(button)
     return cardDesc
 }
 
-for (dog of data) {
+for (const dog of data) {
    const li = document.createElement('li')
    li.innerHTML = dog.name
    li.setAttribute('class', 'dogs-list__button')
-   li.addEventListener('click', createDogCard) // Change the function to display dog card
+   li.addEventListener('click', () => {
+    createDogCard(dog)
+   })
    myHeaderUnorderedList.append(li)
 }
 
